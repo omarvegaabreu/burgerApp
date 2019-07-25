495230d0a9b969060bdd31bfae357d2572ef4c80
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-07-24 21:29:54 -0400

    working app

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
-//Importing dependencies
+// Importing dependencies
 const express = require("express");
 const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 
-//Routes
+// Importing files
 const routes = require("./routes/handlers");
 
-//Express application
 const PORT = process.env.PORT || 9001;
 const app = express();
 
-//body parser and methodOverride middleware
+app.use(express.static(process.cwd() + "/public/..")); // serving static files
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride("_method"));
 
-//handlebars middleware
+// Configure Express Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//app use routes
 app.use("/", routes);
 
-//Setting up the server
 app.listen(PORT, () => {
   console.log(`Server is starting at PORT ${PORT}`);
 });
